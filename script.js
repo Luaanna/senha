128a520a513232f5a4937c2673d921d350c74e01
Luaanna <142031681+Luaanna@users.noreply.github.com>
2025-08-01 11:43:26 -0300

    ok

--- script.js
+++ script.js
@@ -19,10 +19,31 @@ function diminuiTamanho(){
      numeroSenha.textContent = tamanhoSenha;
      geraSenha();
 }
+
 function aumentaTamanho(){
     if(tamanhoSenha < 20){
         tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
+}
+function geraSenha(){
+    let alfabeto = '';
+    if (checkbox[0].checked)alfabeto+=letrasMaiusculas;
+    if (checkbox[1].checked)alfabeto+=letrasMinusculas;
+    if (checkbox[2].ckecked)alfabeto+=numeros;
+    if (checkbox[3].checked)alfabeto+=simbolos;
+
+    if(alfabeto.length===0){
+        campoSenha.values = '';
+        classificaSenha(1)
+        return;
+    }
+    let senha= '';
+    for(let i=0; i < tamanhoSenha; i++);{
+    const numeroAleatorio = Math.floor(Math.random()* alfabeto.length);
+    senha+=alfabeto[numeroAleatorio];}
+    
+    campoSenha.value=senha;
+    classificaSenha(alfabeto.length);
 }
